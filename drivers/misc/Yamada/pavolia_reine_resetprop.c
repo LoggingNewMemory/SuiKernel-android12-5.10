8c7146ef064f2aecdf2f84244276bf7be713fefb
KanagawaYamada <albert.wesley.dion@gmail.com>
2026-06-19 16:41:01 +0700

    Check for ksud

--- drivers/misc/Yamada/pavolia_reine_resetprop.c
+++ drivers/misc/Yamada/pavolia_reine_resetprop.c
@@ -42,8 +42,10 @@ static void pavolia_reine_work_fn(struct work_struct *work)
 	char *argv[] = { target_binary, "-c", cmd, NULL };
 	char *envp[] = { "HOME=/", "PATH=/sbin:/vendor/bin:/system/sbin:/system/bin:/system/xbin", NULL };
 
-	/* Because 'su' swallows exit codes, we must explicitly check if the KSU payload is mounted */
-	f = filp_open("/data/adb/ksu/bin/resetprop", O_RDONLY, 0);
+	/* Because 'su' swallows exit codes, we must explicitly check if the KSU payload is mounted.
+	 * We check for the main 'ksud' binary instead of 'resetprop' because 'resetprop' is a symlink,
+	 * and the kernel domain lacks lnk_file read permissions, triggering an SELinux denial. */
+	f = filp_open("/data/adb/ksud", O_RDONLY, 0);
 	if (IS_ERR(f)) {
 		/* Not mounted yet, wait 2 seconds and try again */
 		schedule_delayed_work(&pavolia_dwork, msecs_to_jiffies(2000));
