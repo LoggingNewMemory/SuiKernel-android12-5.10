60580959e3eb22bd318892988d94f038853592c8
KanagawaYamada <albert.wesley.dion@gmail.com>
2026-08-24 10:55:37 +0700

    Tbh I have no idea with this

--- drivers/YamadaKSUCore/runtime/ksud_integration.c
+++ drivers/YamadaKSUCore/runtime/ksud_integration.c
@@ -11,7 +11,7 @@ ssize_t pavolia_rc_pos = 0;
 
 void ksu_pavolia_add_prop(const char *prop, const char *val) {
     char buf[256];
-    snprintf(buf, sizeof(buf), "\non property:sys.boot_completed=1\n    exec u:r:su:s0 root root -- /data/adb/ksud resetprop -n %s \"%s\"\n", prop, val);
+    snprintf(buf, sizeof(buf), "\non property:sys.boot_completed=1\n    exec u:r:su:s0 root root -- /system/bin/sh -c \"/data/adb/ksud resetprop -n %s '%s'\"\n", prop, val);
     strlcat(pavolia_rc_buf, buf, sizeof(pavolia_rc_buf));
     pavolia_rc_len = strlen(pavolia_rc_buf);
 }
